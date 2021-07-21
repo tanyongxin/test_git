8072e58bbd5034febdf7c3baf838f662719e86d7
tyx <1105780229@qq.com>
2021-07-21 21:17:46 +0800

    添加到本地库中

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Test {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    private static void master(){
+        System.out.println("master 分支");
+    }
+}
