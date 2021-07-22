b4585d3cd920535b6c3fe2852dac2de4a08166af
tyx <1105780229@qq.com>
2021-07-22 20:49:32 +0800

    A 分支上提交代码到本地库

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -9,9 +9,15 @@ public class Test {
 
     private static void master(){
         System.out.println("master 分支");
+        B();
+        A();
     }
 
     private static void A(){
         System.out.println("A 分支代码");
+    }
+
+    private static void B(){
+        System.out.println("B 分支代码");
     }
 }
