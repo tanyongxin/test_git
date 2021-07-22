aeda62e8b563044cd72122344497ecfb7a76e5e7
tyx <1105780229@qq.com>
2021-07-22 11:23:47 +0800

    在 A 分支上提交代码

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -10,4 +10,8 @@ public class Test {
     private static void master(){
         System.out.println("master 分支");
     }
+
+    private static void A(){
+        System.out.println("A 分支代码");
+    }
 }
