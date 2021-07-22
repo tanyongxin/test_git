7e0c1700b71a3db28dadc3620fb5c9df2499d3bf
tyx <1105780229@qq.com>
2021-07-22 11:47:25 +0800

    在 B 分支上提交代码

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -9,9 +9,14 @@ public class Test {
 
     private static void master(){
         System.out.println("master 分支");
+        B();
     }
 
     private static void A(){
         System.out.println("A 分支代码");
+    }
+
+    private static void B(){
+        System.out.println("B 分支代码");
     }
 }
