3045f78c63eccb756e6909341902962ca6665458
tyx <1105780229@qq.com>
2023-10-22 20:35:28 +0800

    A 分支增加方法重载

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -17,6 +17,11 @@ public class Test {
         System.out.println("A 分支代码");
     }
 
+    private static void A(int i){
+        System.out.println("A 分支代码，增加重载方法");
+    }
+
+
     private static void B(){
         System.out.println("B 分支代码");
     }
